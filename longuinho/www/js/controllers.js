76151fefef9ca441ec2d6db842d024fabf0535ab
glairtonbatman@gmail.com <glairtonbatman@gmail.com>
2016-04-15 20:13:10 -0300

    Add de campo para imagens

--- longuinho/www/js/controllers.js
+++ longuinho/www/js/controllers.js
@@ -43,17 +43,17 @@ angular.module('starter.controllers', [])
 
 .controller('PrincipalCtrl', function($scope) {
   $scope.principal = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 7},
-    { title: 'Cowbell', id: 8},
-    { title: 'Cowbell', id: 9},
-    { title: 'Cowbell', id: 10},
-    { title: 'Cowbell', id: 11},
-    { title: 'Cowbell', id: 12}
+    { title: 'Guarda-Chuva', tipo: 'Achado', imag: 'http://images.onccc.com/i002/2014/08/18/79/small_efb950bc7dc079b0867529a0adf6b0e3.jpg' ,id: 1 },
+    { title: 'RG', tipo: 'Perdido',imag: 'http://www.a4pes.com.br/wp-content/uploads/2013/03/rgs.jpg' , id: 2 },
+    { title: 'Cartão do RU', tipo: 'Achado', imag: 'https://www.algorix.com/languages/pt/Images/card6b.gif' ,id: 3 },
+    { title: 'Moto G', tipo: 'Achado',imag: 'http://www.datacillcom.com.br/store/img/128x128/smartphone-motorola-moto-g-2-chip-4g-xt-1078_16090.jpg' , id: 4 },
+    { title: 'Dinheiro', tipo: 'Achado',imag: 'https://33.media.tumblr.com/avatar_1fec673fd256_128.png' ,id: 5 },
+    { title: 'Chave de carro', tipo: 'Perdido', imag: 'http://www.mschaves.com.br/wp-content/uploads/2015/04/131_v1-128x128.jpeg' ,id: 7},
+    { title: 'Livro de Fisica 1', tipo: 'Achado', imag: 'http://d19qz1cqidnnhq.cloudfront.net/imagens/capas/_0e0a9b31223ab359d9b5a219e16d47fdeb0768a6.jpg' ,id: 8},
+    { title: 'Oculos', tipo: 'Perdido', imag: 'http://havan.vteximg.com.br/arquivos/ids/2536860-128-128/Oculos-de-Sol-Gatinho-Retro-Espelhado_0.jpg' ,id: 9},
+    { title: 'Fones', tipo: 'Perdido', imag: 'http://litbimg3.rightinthebox.com/images/m/201503/zcedfe1425623003789.jpg' ,id: 10},
+    { title: 'Estojo', tipo: 'Perdido', imag: 'http://mlb-s2-p.mlstatic.com/estojo-escolar-skelanimals-animal-caveira-caveirinha-23015-MLB20240224293_022015-O.jpg' ,id: 11},
+    { title: 'Jaleco', tipo: 'Perdido', imag: 'http://s3.amazonaws.com/storage.wobiz.com/11/11968/images/Square/1453232965_1cb8eaaac8bd3f8b6cb9cf3a12376418.11968.jpeg' ,id: 12}
   ];
 })
 
